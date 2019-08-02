45744c87bfde9e70efb38f1df8ee020918f0455a
Victor Garcés <victorgarcesgg@gmail.com>
2019-08-02 00:09:05 -0400

    Added API Configuration

--- src/main/java/com/example/books/controllers/BookController.java
+++ src/main/java/com/example/books/controllers/BookController.java
@@ -1,13 +1,25 @@
 package com.example.books.controllers;
 
+import com.example.books.models.Book;
+import com.example.books.repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@CrossOrigin(origins = "*")
 @RestController
+@RequestMapping("/api")
 public class BookController {
 
-    @RequestMapping("/")
-    public String GetBooks(){
-        return "Hello book!!";
+    @Autowired
+    BookRepository bookRepository;
+
+    @GetMapping("/book")
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
     }
 }
