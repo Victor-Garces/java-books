d072b7ebe0653ed57d51a57e9a811e0d6e698161
Victor Garcés <victorgarcesgg@gmail.com>
2019-08-03 01:46:26 -0400

    Added html converter

--- src/main/java/com/example/books/controllers/PageController.java
+++ src/main/java/com/example/books/controllers/PageController.java
@@ -1,12 +1,13 @@
 package com.example.books.controllers;
 
-import com.example.books.dto.BookDto;
 import com.example.books.dto.PageDto;
-import com.example.books.entities.Book;
 import com.example.books.entities.Page;
+import com.example.books.enums.PageFormat;
 import com.example.books.services.PageService;
 import org.modelmapper.ModelMapper;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -22,10 +23,29 @@ public class PageController {
     }
 
     @GetMapping("{id}/page/{pageId}")
-    public PageDto getPageById(@PathVariable(value = "pageId") Long pageId) {
+    public PageDto getPageById(@PathVariable(value = "pageId") Long pageId, @RequestParam(name = "format", required = false) String format) {
         Page page = pageService.getPageById(pageId);
         PageDto pageDto = modelMapper.map(page, PageDto.class);
+
+        if(Objects.equals(PageFormat.html.toString(), format)) {
+            String html = getPageHtmlFormat(pageDto.getContent());
+            pageDto.setContent(html);
+        }
+
         return pageDto;
+    }
+
+    private String getPageHtmlFormat(String content) {
+        StringBuilder htmlBuilder = new StringBuilder();
+        htmlBuilder.append("<html><head></head><body>");
+        String[] paragrahps = content.split("([\\r\\n])");
+        for (String paragrahp: paragrahps) {
+            if(!paragrahp.equals("")){
+                htmlBuilder.append("<p>").append(paragrahp).append("</p>");
+            }
+        }
+        htmlBuilder.append("</body></html>");
+        return htmlBuilder.toString();
     }
 
 }
---
+++ src/main/java/com/example/books/enums/PageFormat.java
@@ -0,0 +1,6 @@
+package com.example.books.enums;
+
+public enum PageFormat {
+    html,
+    plain_text
+}
